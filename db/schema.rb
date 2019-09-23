4793959fbc18e75688a528ada1a8c44d309ffe05
RyuichiMatsuda <pirorin2810@gmail.com>
2019-09-23 19:00:07 +0900

    basic_authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_22_034547) do
+ActiveRecord::Schema.define(version: 2019_09_21_113259) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "name", null: false
@@ -18,25 +18,6 @@ ActiveRecord::Schema.define(version: 2019_09_22_034547) do
     t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "kanjiname", null: false
-    t.string "kananame", null: false
-    t.string "nickname", null: false
-    t.string "email", default: "", null: false
-    t.string "password", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "birthday", null: false
-    t.text "image", null: false
-    t.text "profile", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
