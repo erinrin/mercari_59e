2e3c1dbcd04de887f122c19faebf18617dd4a905
erinrin <happyending24.7@gmail.com>
2019-09-24 12:50:50 +0900

    change_schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_21_113259) do
+ActiveRecord::Schema.define(version: 2019_09_23_153609) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "name", null: false
