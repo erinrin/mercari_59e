dd5bc61c351ab8bb154d9d9723ec63fef8207741
RyuichiMatsuda <pirorin2810@gmail.com>
2019-09-26 14:51:16 +0900

    search view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
  def index
  end
+ def search
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :users
+  get 'items#search' => 'items#search'
 end
