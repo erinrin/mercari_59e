948a3df7579555342f58e5e6785f89e67a5b84da
NaokiSawamura <n.sawamura48@gmail.com>
2019-09-28 14:43:26 +0900

    add login select view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     :omniauth_callbacks =>  "users/omniauth_callbacks"
   }
   root to: 'items#index'
-  resources :users, :items
+  resources :users
+  get 'users#sign_in_select' => 'users#signUp'
+  resources :items
   get 'items#search' => 'items#search'
 end
