4650eb9bdabc09964cf1b0420a441fac84746571
Your Name <you@example.com>
2019-09-28 18:04:00 +0900

    change controller and route

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,26 @@
 class ItemsController < ApplicationController
  def index
  end
+
  def search
  end
+
  def new
   redirect_to new_user_session_path unless user_signed_in?
+  @item = current_user.items.new
  end
+
+ def create
+  @item = Item.create
+ end
+
+ def show
+ end
+
+ def edit
+ end
+
+ def update
+ end
+
 end
