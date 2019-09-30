8f80a72f9f9156beb1c7ec2094d5a2d0bc6c4b01
Your Name <you@example.com>
2019-09-30 20:02:59 +0900

    add item new function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,11 +8,24 @@ class ItemsController < ApplicationController
  def new
   redirect_to new_user_session_path unless user_signed_in?
   @item = current_user.items.new
+  @item.images.build
+  @category_parent_array = ["---"]
+        Category.where(ancestry: nil).each do |parent|
+          @category_parent_array << parent.name
+        end
  end
 
  def create
-  @item = Item.create
- end
+  @item = Item.new(item_params)
+  if @item.save!
+    params[:images][:image_url].each do |image|
+      @item.images.create!(image: image, item_id: @item.id)
+    end
+    redirect_to root_path
+  else
+    redirect_to new_items_path
+  end
+end
 
  def show
  end
@@ -22,5 +35,18 @@ class ItemsController < ApplicationController
 
  def update
  end
+
+  def get_category_children
+  @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+  end
+
+  def get_category_grandchildren
+  @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :description, :seller_id, :buyer_id, :quality, :fee, :sendmethod, :senddate, :region, :category, images_attributes: [:image]).merge(seller_id: current_user.id)
+  end
 
 end
