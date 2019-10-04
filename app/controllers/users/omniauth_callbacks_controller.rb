4f00b4de5563c197637e9699352d912d6c4518e5
Your Name <you@example.com>
2019-10-04 18:55:24 +0900

    revise omniauth and footer

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,7 +14,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
-      redirect_to new_user_registration_path
+      render 'devise/registrations/new'
     end
   end
 
---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,4 @@
+Recaptcha.configure do |config|
+  config.site_key = '6LeqGroUAAAAADWqWYIxKA-jx4hojVNJ28HcYrLR'
+  config.secret_key = '6LeqGroUAAAAAHQVL0OHTtXKsE2rZvrtgKepHrnx'
+end
