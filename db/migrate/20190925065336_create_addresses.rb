065d0b2d0cf40ac99985303e7af9f65709b69b33
Your Name <you@example.com>
2019-10-09 21:06:32 +0900

    add-after-registeration-function

--- db/migrate/20190925065336_create_addresses.rb
+++ db/migrate/20190925065336_create_addresses.rb
@@ -6,12 +6,12 @@ class CreateAddresses < ActiveRecord::Migration[5.2]
       t.string :name_last,     null: true, foreign_key: true 
       t.string :name_kana_first, null: true, foreign_key: true
       t.string :name_kana_last, null: true, foreign_key: true 
-      t.integer :post_number,  null: false
+      t.string :post_number,  null: false
       t.integer :prefecture,   null: false
       t.string :city,          null: false
-      t.text :house_number,    null: false
+      t.string :house_number,    null: false
       t.string :building,      null: true
-      t.integer :phone_number, null: false
+      t.string :phone_number, null: false
       t.timestamps
     end
   end
