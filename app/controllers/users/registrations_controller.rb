2aaf13ba6593f97cae800a7718863eb43fc7c496
Your Name <you@example.com>
2019-10-11 11:07:49 +0900

    add-recaptcha-to-signup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,16 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+
   def after_sign_up_path_for(resource)
     addphone_user_path(current_user.id)
   end
+
+  def create
+    if verify_recaptcha
+      super
+    else
+      self.resource = resource_class.new
+      respond_with_navigational(resource) { render :new }
+    end
+  end
+  
 end
